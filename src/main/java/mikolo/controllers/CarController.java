e2e7b4bb036f3a786b6f0eb6e309bdbc2b7daf25
Mikolaj Kedzierski <mikked@poczta.onet.pl>
2019-09-12 14:19:50 +0200

    iteration on collections added

--- src/main/java/mikolo/controllers/CarController.java
+++ src/main/java/mikolo/controllers/CarController.java
@@ -1,5 +1,8 @@
 package mikolo.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +13,14 @@ public class CarController {
 	
 	@GetMapping({"", "car"})
 	public String showCar(Model model) {
-		Car car = new Car("Skoda", "Fabia");
-		model.addAttribute("car", car);
+		List<Car>carList = Arrays.asList(
+		new Car("Skoda", "Fabia"),
+		new Car("Volkswagen", "Golf"),
+		new Car("BMW", "M5"),
+		new Car("Peugeot", "308"),
+		new Car("Ford", "Focus"));
+	
+		model.addAttribute("cars", carList);
 		return "car";
 	}
 }
