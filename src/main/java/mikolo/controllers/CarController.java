c2db6f594f765a8a4bd0e53eedd0398246c50425
Mikolaj Kedzierski <mikked@poczta.onet.pl>
2019-09-12 13:42:24 +0200

    first commit

---
+++ src/main/java/mikolo/controllers/CarController.java
@@ -0,0 +1,20 @@
+package mikolo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import mikolo.model.Car;
+
+@Controller
+public class CarController {
+	
+	@GetMapping({"", "car"})
+	public String showCar(Model model) {
+		Car car = new Car("Skoda", "Fabia");
+
+		model.addAttribute("car", car);
+		return "car";
+	}
+
+}
---
+++ src/main/java/mikolo/model/Car.java
@@ -0,0 +1,17 @@
+package mikolo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+public class Car {
+	
+	private String mark;
+	private String model;
+
+}
